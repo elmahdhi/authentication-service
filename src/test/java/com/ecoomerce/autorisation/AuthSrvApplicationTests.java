d1a2f9cd81e6c17f52e76236b9296d0af1718b4f
Walid Mahdhi <walidelmahdhi@gmail.com>
2024-01-24 12:08:47 +0100

    add pom xml file

---
+++ src/test/java/com/ecoomerce/autorisation/AuthSrvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecoomerce.autorisation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthSrvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
